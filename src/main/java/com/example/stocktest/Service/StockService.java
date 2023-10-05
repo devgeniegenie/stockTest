f3ad3cfd466748236e78ed6b71860d78deeb2f18
Genie <devgeniegenie@gmail.com>
2023-10-06 00:21:09 +0900

    레이스 컨디션 발생시키는 테스트코드 작성

--- src/main/java/com/example/stocktest/Service/StockService.java
+++ src/main/java/com/example/stocktest/Service/StockService.java
@@ -4,6 +4,8 @@ import com.example.stocktest.Domain.Stock;
 import com.example.stocktest.Repository.StockRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class StockService {
 
--- src/test/java/com/example/stocktest/Service/StockServiceTest.java
+++ src/test/java/com/example/stocktest/Service/StockServiceTest.java
@@ -8,7 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.transaction.Transactional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StockServiceTest {
@@ -37,6 +42,32 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0
+        assertEquals(0, stock.getQuantity());
+        //실패 (레이스 컨디션 : 둘 이상의 Thread가 공유 데이터에 엑세스 할 수 있고 동시에 변경을 하려고 할때 발생하는 문제)
+        //Thread1이 1 줄이고 갱신되기 이전에 Thread2가 가져가서 갱신되기 전의 값을 가져와서 갱신함
     }
 
 }
